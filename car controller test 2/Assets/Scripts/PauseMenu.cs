3e6d805d83905ab4015a272d11977e85e332a4e1
gitWyatt <81323361+gitWyatt@users.noreply.github.com>
2022-02-25 23:33:35 -0500

    first build uploaded

--- car controller test 2/Assets/Scripts/PauseMenu.cs
+++ car controller test 2/Assets/Scripts/PauseMenu.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
@@ -100,6 +101,10 @@ public class PauseMenu : MonoBehaviour
             carController.frontWheelDrive = true;
             carController.rearWheelDrive = true;
         }
+    }
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
     }
     public void QuitGame()
     {
